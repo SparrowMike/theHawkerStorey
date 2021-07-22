8bb4eaf62ceaa4841a9f5f0258f58ee1919b2ddc
sparrowmike <michal.m_wroblewski@hotmail.com>
2021-07-22 19:38:11 +0800

    added dummy controller for express

---
+++ README.md
@@ -0,0 +1,2 @@
+react running on localhost:3000
+express running on localhost:4000/v1/dummy
--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 const express = require("express");
-require("dotenv").config();
-
 const app = express();
-const PORT = process.env.PORT;
+const mongoose = require("mongoose");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+const dishes = require("./models/dishes.js");
+
+require("dotenv").config();
+const PORT = process.env.PORT;
+const mongodbURI = process.env.MONGODB_URI;
+
+app.use(cors());
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(express.json());
+app.use(express.static("public"));
+
+const dummyController = require("./controllers/dummy.js");
+app.use("/v1/dummy", dummyController);
 
 app.listen(PORT, () => {
   console.log("Listening on the port", PORT);
+});
+mongoose.connect(mongodbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
 });
