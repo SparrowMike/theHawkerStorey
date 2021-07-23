08b0c9b3c9c53ddb628be8ce11064daf01bf7317
David Liew <davidliew@davemac.local>
2021-07-23 13:54:12 +0800

    dave changes

--- models/hawkerCentre.js
+++ models/hawkerCentre.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
-const hawkerCentreSchema = mongoose.Schema({});
+const hawkerCentreSchema = mongoose.Schema({
+  name_of_centre: {{type: String, required: true}}
+});
 
 module.exports = mongoose.model("HawkerCentre", hawkerCentreSchema);
