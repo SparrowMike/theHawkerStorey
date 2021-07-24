95f921d363202a8c9da2d4a23492964ca7960f70
Charmaine Chan <chachan@Charmaines-MacBook-Pro.local>
2021-07-24 22:12:57 +0800

    corrected posts users dishes schema

--- models/users.js
+++ models/users.js
@@ -2,15 +2,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 
 const usersSchema = new Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: String },
+  email: { type: String, unique: true },
   user_postal_code: { type: String },
-  favourite_dishes: { Array },
-  posts_history: { type: String }, //! Reference (POSTS id)
+  favourite_dishes: [String],
+  posts_history: {type: Schema.Types.ObjectId, ref: "Posts"}, //! Reference (POSTS id)
 
   //! + cuisine that they like (query from DISHES cuisine)
 });
 
-const Users = mongoose.model("Users", usersSchema)
+const Users = mongoose.model("Users", usersSchema);
+
 module.exports = Users;
+
