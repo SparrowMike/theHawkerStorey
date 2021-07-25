b71f5bc7bc149c01e08c6c808f86394eaabdb222
sparrowmike <michal.m_wroblewski@hotmail.com>
2021-07-25 19:54:41 +0800

    adding necessary for heroku deployment

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const app = express();
 const mongoose = require("mongoose");
 const session = require("express-session");
 const cors = require("cors");
-const { cloudinary } = require('./utils/cloudinary')
+const { cloudinary } = require("./utils/cloudinary");
+const path = require("path");
 
 // =======================================
 //              CONFIGURATIONS
 // =======================================
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 const mongodbURI = process.env.MONGODB_URI;
-
 
 // =======================================
 //        BODY PARSER, MIDDLEWARE
@@ -31,8 +31,8 @@ app.use(
 );
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static("./client/build"));
 const methodOverride = require("method-override");
-
 
 // =======================================
 //            MONGOOSE CONNECTION
@@ -54,7 +54,6 @@ mongoose.connection.once("open", () => {
   console.log("connected to mongo");
 });
 
-
 // =======================================
 //         CONTROLLERS/ROUTES
 // =======================================
@@ -63,7 +62,7 @@ const postsController = require("./controllers/posts.js");
 app.use("/v1/posts", postsController);
 
 const hawkersController = require("./controllers/hawkers");
-app.use("/v1/hawkers", hawkersController)
+app.use("/v1/hawkers", hawkersController);
 
 const dishesController = require("./controllers/dishes.js");
 app.use("/v1/dishes", dishesController);
@@ -71,30 +70,25 @@ app.use("/v1/dishes", dishesController);
 const usersController = require("./controllers/users.js");
 app.use("/v1/users", usersController);
 
-
-
 // =======================================
 //              LISTENER
 // =======================================
-//! temporary area for testing cloudinary upload. 
+//! temporary area for testing cloudinary upload.
 //! to change fetch route in ImageUpload.js when we move the code from server.js to posts controller
-app.post('/upload', async (req, res) => {
+app.post("/upload", async (req, res) => {
   try {
     const fileStr = req.body.data;
-    const uploadedResponse = await cloudinary.uploader.upload(
-      fileStr, {
-        upload_preset: "hawkerstorey-preset"
-      });
-    res.json({ msg: uploadedResponse })
+    const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: "hawkerstorey-preset",
+    });
+    res.json({ msg: uploadedResponse });
     console.log("WE SENT IT TO THE CLOUD!!", uploadedResponse);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ err: 'Uh oh. Something went wrong' });
+    console.log(error);
+    res.status(500).json({ err: "Uh oh. Something went wrong" });
   }
-})
-
+});
 
 app.listen(PORT, () => {
   console.log("Listening on the port", PORT);
 });
-
