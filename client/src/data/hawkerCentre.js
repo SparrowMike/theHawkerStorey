3594a48e697985460af5240988a595d4aec2dc72
sparrowmike <michal.m_wroblewski@hotmail.com>
2021-07-26 17:34:40 +0800

    fixed the bug

--- client/src/data/hawkerCentre.js
+++ client/src/data/hawkerCentre.js
@@ -2,18 +2,14 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 const HawkerCentre = () => {
-
-const {data} = useQuery("hawkercentres", () =>
-    axios("/v1/hawkers")
-  );
+  const { data } = useQuery("hawkercentres", () => axios("/v1/hawkers"));
 
   // if (error) return <h1>Error, {error.message}, try again </h1>;
   // if (isLoading) return <h1>Loading....</h1>;
 
-  console.log("this is the data", data.data);
+  console.log("this is the data", data);
 
-  return <></>
- 
+  return <></>;
 };
 
 export default HawkerCentre;
