612eeb5ff271e0e303aa090209822e80b5e794f0
sparrowmike <michal.m_wroblewski@hotmail.com>
2021-07-26 10:49:06 +0800

    slight adjustments within app and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
@@ -9,22 +9,16 @@ function App() {
   return (
     <Router>
       <Switch>
-
+        <Navbar />
         <Route path="/" exact>
-          <Navbar />
           <Main />
         </Route>
-
         <Route path="/signup">
-          <Navbar />
           <SignUp />
         </Route>
-
         <Route path="/login">
-          <Navbar />
           <SignIn />
         </Route>
-        
       </Switch>
     </Router>
   );
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -24,7 +24,7 @@ import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
 
-import Post from "./post";
+import Post from "./Post";
 
 const drawerWidth = 240;
 
