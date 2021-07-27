3b155c494cfbdb79399e008ef06ae4807ecff34b
sparrowmike <michal.m_wroblewski@hotmail.com>
2021-07-27 13:27:38 +0800

    updated front page styling

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -11,38 +11,34 @@ const useStyles = makeStyles((theme) => ({
   icon: {
     marginRight: theme.spacing(2),
   },
+
   heroContent: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6),
-    position: "relative",
-    zIndex: "1",
-  },
-  image: {
-    // backgroundImage:
-    //   "url(https://www.visitsingapore.com/singapore-itineraries/singapore-food-paradise-a-three-day-guide/_jcr_content/par-carousel/carousel_detailpage/carousel/item0.thumbnail.carousel-img.740.416.jpg)",
     backgroundImage:
-      "url(https://www.visitsingapore.com/editorials/did-you-know-foodies/_jcr_content.renderimage.carousel.rect.835.470.jpg) center center",
+      // " linear-gradient(rgba(255,255,255,0.4), rgba(255,255,255,0.4)), url(https://www.visitsingapore.com/editorials/did-you-know-foodies/_jcr_content.renderimage.carousel.rect.835.470.jpg)",
+      // " linear-gradient(rgba(255,255,255,0.4), rgba(255,255,255,0.4)), url(https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80)",
+      " linear-gradient(rgba(255,255,255,0.4), rgba(255,255,255,0.4)), url(https://www.visitsingapore.com/singapore-itineraries/singapore-food-paradise-a-three-day-guide/_jcr_content/par-carousel/carousel_detailpage/carousel/item0.thumbnail.carousel-img.740.416.jpg)",
+
+    //* alternative picture
+
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
-    opacity: "0.4",
-    position: "absolute",
-    zIndex: "-1",
-    // top: "0",
-    // bottom: "0",
-    // left: "0",
-    // right: "0",
-    // background: url(test.jpg) center center;
-
-    width: "100%",
-    height: " 100%",
   },
 
   heroButtons: {
     marginTop: theme.spacing(4),
+    color: theme.palette.common.white,
   },
 
   header: {
     color: theme.palette.common.white,
+    textShadow: theme.shadows[10],
+  },
+
+  button: {
+    // color: theme.palette.primary,
+    // background: theme.palette.secondary,
   },
 }));
 
@@ -53,7 +49,6 @@ export default function Main() {
     <React.Fragment>
       <CssBaseline />
       <main>
-        <div className={classes.image} />
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography
@@ -78,12 +73,12 @@ export default function Main() {
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justifyContent="center">
                 <Grid item>
-                  <Button variant="contained" color="primary">
+                  <Button variant="contained" className={classes.button}>
                     Main call to action
                   </Button>
                 </Grid>
                 <Grid item>
-                  <Button variant="outlined" color="primary">
+                  <Button variant="outlined" className={classes.button}>
                     Secondary action
                   </Button>
                 </Grid>
