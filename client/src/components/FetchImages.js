4be2638fd56dace4870fd3746daf4bcb13274db0
sparrowmike <michal.m_wroblewski@hotmail.com>
2021-07-27 21:47:04 +0800

    on click modal

--- client/src/components/FetchImages.js
+++ client/src/components/FetchImages.js
@@ -6,6 +6,7 @@ import {
   Modal,
   Backdrop,
   Fade,
+  Typography,
 } from "@material-ui/core";
 
 import StackGrid from "react-stack-grid";
@@ -32,6 +33,7 @@ const useStyles = makeStyles((theme) => ({
     padding: "40px",
   },
   modal: {
+    // overflow: "scroll",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
@@ -49,13 +51,15 @@ require("dotenv").config();
 const FetchImages = () => {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
-
   const [imageIds, setImageIds] = useState();
+  const [modal, setModal] = useState("");
 
-  const handleOpen = () => {
+  //*===============OPEN MODAL==============
+  const handleOpen = (e) => {
     setOpen(true);
+    console.log(e);
+    setModal(e);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
@@ -85,7 +89,7 @@ const FetchImages = () => {
               <Image
                 className={classes.cardMedia}
                 key={index}
-                onClick={handleOpen}
+                onClick={() => handleOpen(imageId)}
                 cloudName={"hawkerstorey"}
                 publicId={imageId}
                 crop="scale"
@@ -103,7 +107,19 @@ const FetchImages = () => {
           }}
         >
           <Fade in={open}>
-            <div className={classes.paper}></div>
+            <div className={classes.paper}>
+              <Image
+                // key={index}
+                // onClick={() => handleOpen(imageId)}
+                cloudName={"hawkerstorey"}
+                publicId={modal}
+                crop="scale"
+                width={300}
+              />
+              <Typography variant="h5" align="center">
+                Some text will go here right?
+              </Typography>
+            </div>
           </Fade>
         </Modal>
       </Container>
