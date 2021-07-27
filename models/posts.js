f6aee0a8c234fe9c22a3a9d6c360eb63b883a218
sparrowmike <michal.m_wroblewski@hotmail.com>
2021-07-27 11:16:45 +0800

    done CRUD for the post, updated the schema

--- models/posts.js
+++ models/posts.js
@@ -8,9 +8,9 @@ const postsSchema = new Schema({
 
   cloudinary_id: String,
 
+  dishes_id: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
   posted_by: { type: Schema.Types.ObjectId, ref: "Users" }, //"Users" references User Schema
-  dishes_id: [{ type: Schema.Types.ObjectId, ref: "Dishes", required: true }],
 
   //! + hawker centre they are referring to (query from HAWKER CENTRE id)
   //! + hawker stall they are referring to (query from HAWKER STALL id)
