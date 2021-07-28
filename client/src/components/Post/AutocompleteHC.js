592af85f2f4c09a2d43b0580cd8b47b3edcc0aa6
David Liew <daveliew@gmail.com>
2021-07-28 15:50:38 +0800

    added sort for HC names options in autocomplete

--- client/src/components/Post/AutocompleteHC.js
+++ client/src/components/Post/AutocompleteHC.js
@@ -4,23 +4,32 @@ import axios from "axios";
 import { Autocomplete } from "@material-ui/lab/";
 import { TextField } from "@material-ui/core/";
 
-const AutocompleteHC = ({ setHawkerCentre }) => {
+const AutocompleteHC = ({ setHawkerCentre, setHawkerStall, setDishName }) => {
   //* Fetching of hawker centres data
   const { data } = useQuery(
-    "hawkercentre-list",
+    ["hawkercentre-list"],
     async () => await axios("/v1/hawkers/")
   );
 
-  const hcList = data?.data?.map((hc) => hc.name);
+  const hcList = data?.data
+    ?.map((hc) => hc.name)
+    ?.sort((a, b) => a.localeCompare(b, { ignorePunctuation: true }));
 
   return (
     <Autocomplete
       id="Hawker Centre"
+      autoHighlight
+      clearOnEscape
+      //   options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+      //   groupBy={(option) => option.firstLetter}
+      //   getOptionLabel={(option) => option.title}
       options={hcList}
+      //   groupBy={(option) => option.firstLetter}
       getOptionLabel={(option) => option}
       onChange={(event, newValue) => {
-        console.log("CHOSE THIS VALUE", newValue);
         setHawkerCentre(newValue);
+        setHawkerStall("");
+        setDishName("");
       }}
       renderInput={(params) => (
         <TextField {...params} label="Hawker Centre" variant="outlined" />
