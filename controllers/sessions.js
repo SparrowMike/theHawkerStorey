7a300ba118851236f9bbfa2553dc897d3b4b8a39
David Liew <daveliew@gmail.com>
2021-07-28 22:26:26 +0800

    removed auth token function and moved to utils

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -4,22 +4,6 @@ const router = express.Router();
 const Users = require("../models/users");
 const jwt = require("jsonwebtoken");
 
-//* authenticate token middleware
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  console.log(token);
-  if (token === null) return res.sendStatus(401); //no token received
-
-  // process the secret we receive
-  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-    if (err) return res.sendStatus(403); //token no longer valid
-    //user can now proceed
-    req.user = user;
-    next();
-  });
-};
-
 //localhost:4000/v1/sessions
 router.get("/", (req, res) => {
   res.send("sessions");
