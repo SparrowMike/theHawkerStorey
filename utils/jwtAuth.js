74f701d57e36b1457d7bea193d25ef985b04e55e
daveliew <daveliew@gmail.com>
2021-07-29 00:47:11 +0800

    updated package.json

---
+++ utils/jwtAuth.js
@@ -0,0 +1,20 @@
+// require("dotenv").config();
+// const jwt = require("jsonwebtoken");
+
+// //* authenticate token middleware
+// const jwtAuth = (req, res, next) => {
+//   const authHeader = req.headers["authorization"];
+//   const token = authHeader && authHeader.split(" ")[1];
+//   console.log(token);
+//   if (token === null) return res.sendStatus(401); //no token received
+
+//   // process the secret we receive
+//   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+//     if (err) return res.sendStatus(403); //token no longer valid
+//     //user can now proceed
+//     req.user = user;
+//     next();
+//   });
+// };
+
+// module.exports = jwtAuth;
