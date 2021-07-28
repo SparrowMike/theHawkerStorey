245e3a4e77870033a7dae86798c8d70b939325a6
David Liew <daveliew@gmail.com>
2021-07-28 15:51:44 +0800

    added sort for HS name options

--- client/src/components/Post/AutocompleteHS.js
+++ client/src/components/Post/AutocompleteHS.js
@@ -7,17 +7,30 @@ import TextField from "@material-ui/core/TextField";
 const AutocompleteHS = ({ hawkerCentre, setHawkerStall }) => {
   //* Fetching of hawker stalls
   const { data, isLoading } = useQuery(
-    "hawkerstalls",
+    ["hawkerstalls", hawkerCentre],
     async () => await axios(`/v1/hawkers/${hawkerCentre}`)
   );
 
   //* map stall names and pass to autocomplete as options
-  const hsList = data?.data?.hawker_stalls?.map((stall) => stall.name);
+  const hsList = data?.data?.hawker_stalls
+    ?.map((stall) => stall.name)
+    ?.sort((a, b) => a.localeCompare(b, { ignorePunctuation: true }));
 
   return (
     <>
       {isLoading ? (
-        <h1>Fetching Hawker Stall Options</h1>
+        <Autocomplete
+          id="Hawker Stall"
+          disabled
+          options={hsList}
+          getOptionLabel={(option) => option}
+          onChange={(event, newValue) => {
+            setHawkerStall(newValue);
+          }}
+          renderInput={(params) => (
+            <TextField {...params} label="Hawker Stall" variant="outlined" />
+          )}
+        />
       ) : (
         <Autocomplete
           id="Hawker Stall"
