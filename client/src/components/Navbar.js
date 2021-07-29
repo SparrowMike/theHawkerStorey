8877864fb327d7c53c4ca794a22da2b0c41b3fff
SparrowMike <69448825+SparrowMike@users.noreply.github.com>
2021-07-29 11:31:31 +0800

    navbar improvments

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -99,7 +99,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Navbar({ userState }) {
+export default function Navbar({ userState, setUserState }) {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = useState(false);
@@ -136,12 +136,6 @@ export default function Navbar({ userState }) {
             <MenuItem component={RouterLink} to="/">
               <Typography variant="h6">Home</Typography>
             </MenuItem>
-            {/* <MenuItem component={RouterLink} to="/">
-              <Typography variant="h6">Hawker</Typography>
-            </MenuItem>
-            <MenuItem component={RouterLink} to="/">
-              <Typography variant="h6">Cuisine</Typography>
-            </MenuItem> */}
             {userState.accessToken && (
               <MenuItem onClick={handleOpenPost}>
                 <Typography variant="h6">Create Post</Typography>
@@ -200,17 +194,6 @@ export default function Navbar({ userState }) {
         </div>
         <Divider />
 
-        <List>
-          <ListItem
-            button
-            component={RouterLink}
-            onClick={handleDrawerClose}
-            to="/"
-          >
-            <ListItemText primary="Home" />
-          </ListItem>
-        </List>
-
         {userState.accessToken ? (
           <List>
             <ListItem button onClick={handleOpenPost}>
@@ -219,12 +202,20 @@ export default function Navbar({ userState }) {
             <ListItem button onClick={handleOpenPost}>
               <ListItemText primary="Profile" />
             </ListItem>
-            <ListItem button onClick={handleOpenPost}>
+            <ListItem button onClick={setUserState("")}>
               <ListItemText primary="Logout" />
             </ListItem>
           </List>
         ) : (
           <List>
+            <ListItem
+              button
+              component={RouterLink}
+              onClick={handleDrawerClose}
+              to="/"
+            >
+              <ListItemText primary="Home" />
+            </ListItem>
             <ListItem
               button
               component={RouterLink}
